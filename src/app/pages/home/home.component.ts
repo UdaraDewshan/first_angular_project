18d1ea67c44e55cca3175a3c0ddc896dad3d3030
UdaraDewshan <dewudara@gmail.com>
2026-01-14 00:28:09 +0530

    Add skill section

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -4,10 +4,11 @@ import { HeadSectionComponent } from '../../components/head-section/head-section
 import { AboutSectionComponent } from '../../components/about-section/about-section.component';
 import { EducationSectionComponent } from '../../components/education-section/education-section.component';
 import { ProjectSectionComponent } from '../../components/project-section/project-section.component';
+import { SkillSectionComponent } from '../../components/skill-section/skill-section.component';
 
 @Component({
   selector: 'app-home',
-  imports: [NavBarComponent, HeadSectionComponent,AboutSectionComponent,EducationSectionComponent,ProjectSectionComponent],
+  imports: [NavBarComponent, HeadSectionComponent,AboutSectionComponent,EducationSectionComponent,ProjectSectionComponent,SkillSectionComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
