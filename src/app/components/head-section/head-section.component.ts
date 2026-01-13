ea21cb7ced2a907100de8e5ec3fdaa4c4e7f49fc
UdaraDewshan <dewudara@gmail.com>
2026-01-14 00:11:25 +0530

    Add HeadSection component with template and styles

---
+++ src/app/components/head-section/head-section.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-head-section',
+  imports: [],
+  templateUrl: './head-section.component.html',
+  styleUrl: './head-section.component.css'
+})
+export class HeadSectionComponent {
+
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { NavBarComponent } from '../../components/nav-bar/nav-bar.component';
+import { HeadSectionComponent } from '../../components/head-section/head-section.component';
 
 @Component({
   selector: 'app-home',
-  imports: [NavBarComponent],
+  imports: [NavBarComponent, HeadSectionComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
