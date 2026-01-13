6bffc43a737c871e62ddd11950b5d77f45a27ebb
UdaraDewshan <dewudara@gmail.com>
2026-01-14 00:14:41 +0530

    Add About Section

---
+++ src/app/components/about-section/about-section.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-about-section',
+  imports: [RouterLink],
+  templateUrl: './about-section.component.html',
+  styleUrl: './about-section.component.css'
+})
+export class AboutSectionComponent {
+
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavBarComponent } from '../../components/nav-bar/nav-bar.component';
 import { HeadSectionComponent } from '../../components/head-section/head-section.component';
+import { AboutSectionComponent } from '../../components/about-section/about-section.component';
 
 @Component({
   selector: 'app-home',
-  imports: [NavBarComponent, HeadSectionComponent],
+  imports: [NavBarComponent, HeadSectionComponent,AboutSectionComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
